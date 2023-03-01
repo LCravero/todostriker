fe172e3d491099cb3c9b68e1b1a028f9943975bf
Lucas Cravero <lucas.cravero@mercadolibre.com>
2023-03-01 17:30:44 -0300

    feat: add ESLint, StandardJS style, Husky, lintstaged and commitlint
    Configuring ESLint and add rules
    Add two git hooks actions by Husky
    Configuring lintstaged for some files' extension

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home () {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,75 +15,64 @@ export default function Home() {
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By
+{' '}
+<Image priority alt="Vercel Logo" className={styles.vercelLogo} height={24} src="/vercel.svg" width={100} />
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <Image priority alt="Next.js Logo" className={styles.logo} height={37} src="/next.svg" width={180} />
         <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          <Image priority alt="13" height={31} src="/thirteen.svg" width={40} />
         </div>
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target="_blank"
+          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
+            Docs
+{' '}
+<span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          <p className={inter.className}>Find in-depth information about Next.js features and API.</p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target="_blank"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
+            Templates
+{' '}
+<span>-&gt;</span>
           </h2>
           <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
-          target="_blank"
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           rel="noopener noreferrer"
+          target="_blank"
         >
           <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
+            Deploy
+{' '}
+<span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p className={inter.className}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
