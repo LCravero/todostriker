eff5029fb401093b46fe37c8f88e3b6892d6a319
Lucas Cravero <lucas.cravero@mercadolibre.com>
2023-03-13 16:02:22 -0300

    feat: add @testing-library as devDependency and configuring Jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+module.exports = createJestConfig(customJestConfig)
