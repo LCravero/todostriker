da610bdd0b5feceacad64a7be361aacba35d6573
Lucas Cravero <lucas.cravero@mercadolibre.com>
2023-03-14 17:59:01 -0300

    fix: adjust Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,11 @@
 import {
-  // Avatar,
   Box,
   Button,
-  // Center,
   Flex,
   Link,
-  /* Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList, */
   Stack,
   useColorMode,
   useColorModeValue
-  // useDisclosure
 } from '@chakra-ui/react'
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
@@ -34,52 +26,22 @@ const NavLink = ({ children, href }) => (
 
 export default function Navbar () {
   const { colorMode, toggleColorMode } = useColorMode()
-  // const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} minW="100%" px={4}>
         <Flex alignItems={'center'} h={16} justifyContent={'space-between'}>
           <NavLink href="/"><strong>TODOSTRIKER</strong></NavLink>
-          <NavLink href="/lists">My Lists</NavLink>
+          <Flex gap={'1rem'}>
+            <NavLink href="/lists">My Lists</NavLink>
 
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-
-              {/* <Menu>
-                <MenuButton
-                  as={Button}
-                  cursor={'pointer'}
-                  minW={0}
-                  rounded={'full'}
-                  variant={'link'}
-                >
-                  <Avatar
-                    size={'sm'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
-                  />
-                </MenuButton>
-                <MenuList alignItems={'center'}>
-                  <br />
-                  <Center>
-                    <Avatar
-                      size={'2xl'}
-                      src={'https://avatars.dicebear.com/api/male/username.svg'}
-                    />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>Username</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>My Todos Lists</MenuItem>
-                </MenuList>
-              </Menu> */}
-            </Stack>
+            <Flex alignItems={'center'}>
+              <Stack direction={'row'} spacing={7}>
+                <Button variant="ghost" onClick={toggleColorMode}>
+                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Stack>
+            </Flex>
           </Flex>
         </Flex>
       </Box>
