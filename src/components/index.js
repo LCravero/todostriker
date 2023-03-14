be665ce4612843151627d28cd48be4ccd1f5306b
Lucas Cravero <lucas.cravero@mercadolibre.com>
2023-03-14 18:04:38 -0300

    feat: add TodoContainer to components' barrel file

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import ChakraUIProvider from './ChakraProvider/ChakraUIProvider'
 import Footer from './Footer/Footer'
 import List from './List'
 import Navbar from './Navbar/Navbar'
-import Todo from './Todo/Todo'
-export { ChakraUIProvider, Footer, List, Navbar, Todo }
+import TodoContainer from './Todo/TodoContainer'
+export { ChakraUIProvider, Footer, List, Navbar, TodoContainer }
